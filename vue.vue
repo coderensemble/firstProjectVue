b518531dbbb3e28db85526dbf71bf55a763c26ef
Adrien NEYRON <adrien.neyron@gmail.com>
2024-03-04 15:04:49 +0100

    add vue.vue

---
+++ vue.vue
@@ -0,0 +1,29 @@
+Vue.component("fiche-produit", {
+  template: `
+    <div class="col-3">
+        <p class="jumbotron"> {{ nom }} <br>
+            <button v-on:click="passerCommande(nom)" v-if="role == 'commande'" >Commander !</button>
+            </p>
+            </div>`,
+  props: ["nom", "role"],
+  methods: {
+    passerCommande: function (produit) {
+      this.$emit("commande-passee", produit);
+    },
+  },
+});
+const app = new Vue({
+  router: "#app",
+  data: {
+    user: "Adrien",
+    produits: ["Ultimate", "Course d'orientation", "Cyclisme", "Roller"],
+    commandes: [],
+    affichage: "display-4",
+  },
+  methods: {
+    commander: function (produit) {
+      // console.log(produit)
+      this.commandes.push(produit);
+    },
+  },
+}).$mount('#app');
